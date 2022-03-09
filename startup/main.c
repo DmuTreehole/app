66642cca6f1d3535cd012772e4fda9525c172bd5
Chi-Kai <chikai0307@gmail.com>
2022-03-09 16:36:15 +0800

    fix bugs

--- startup/main.c
+++ startup/main.c
@@ -13,22 +13,33 @@ hi_void init(hi_void)
 {
 	hi_gpio_init();
 	//引脚复用
-	hi_io_set_func(HI_IO_NAME_GPIO_0, HI_IO_FUNC_GPIO_0_PWM3_OUT); 
-    hi_io_set_func(HI_IO_NAME_GPIO_1, HI_IO_FUNC_GPIO_1_PWM4_OUT); 	
-	hi_io_set_func(HI_IO_NAME_GPIO_9, HI_IO_FUNC_GPIO_9_PWM0_OUT); 
-    hi_io_set_func(HI_IO_NAME_GPIO_10, HI_IO_FUNC_GPIO_10_PWM1_OUT); 
+	hi_io_set_func(HI_IO_NAME_GPIO_0, HI_IO_FUNC_GPIO_0_PWM3_OUT);
+	hi_io_set_func(HI_IO_NAME_GPIO_1, HI_IO_FUNC_GPIO_1_PWM4_OUT);
+	hi_io_set_func(HI_IO_NAME_GPIO_9, HI_IO_FUNC_GPIO_9_PWM0_OUT);
+	hi_io_set_func(HI_IO_NAME_GPIO_10, HI_IO_FUNC_GPIO_10_PWM1_OUT);
 	//初始化pwm
 	hi_pwm_init(HI_PWM_PORT_PWM3);
-    hi_pwm_init(HI_PWM_PORT_PWM4);
+	hi_pwm_init(HI_PWM_PORT_PWM4);
 	hi_pwm_init(HI_PWM_PORT_PWM0);
-    hi_pwm_init(HI_PWM_PORT_PWM1);
-
+	hi_pwm_init(HI_PWM_PORT_PWM1);
 }
 
-void Move(void) {
+void Move(void)
+{
+
+	int ret = sem_init(&sem, 0, 0); //初始化信号量 1 并且赋予其资源
+	if (ret < 0)
+	{
+		perror("sem_init");
+		return -1;
+	}
+
 	RunHotspot();
-  Openport();	
-	UdpServer();    
- }
+	Openport();
+	Server();
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+}
 
 SYS_RUN(Move); // BUG不用管 入口程序
--- startup/net.h
+++ startup/net.h
@@ -1,15 +1,18 @@
+#ifndef NET
+#define NET
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
-//#include "lwip/sockets.h"
+#include "lwip/sockets.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
-//#include"supersound.h"
-//#include "motor.h"
-//#include "ohos_init.h"
-//#include "cmsis_os2.h"
+#include "supersound.h"
+#include "motor.h"
+#include "ohos_init.h"
+#include "cmsis_os2.h"
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -19,37 +22,43 @@ int speed = 800;
 char buf[SIZE1];
 pthread_t t1, t2;
 sem_t sem;
-/*
+
 int direct = 0; // 0 直行 -1 后退 1 左转 2右转
-void ChangeSpeed(bool as){
-    if (!as) {
+void ChangeSpeed(bool as)
+{
+    if (!as)
+    {
         speed += 100;
-        if (speed % 100 == 1) {
+        if (speed % 100 == 1)
+        {
             speed -= 1;
         }
-    }else{
+    }
+    else
+    {
         speed -= 100;
-        if(speed < 0 ) {
+        if (speed < 0)
+        {
             speed = 1;
         }
     }
-    printf("改变速度,当前速度为：%d",speed);
-    switch (direct){
+    printf("改变速度,当前速度为：%d", speed);
+    switch (direct)
+    {
     case 0:
-//        go_forward(speed);
+        go_forward(speed);
         break;
     case 1:
-       // go_turnleft(speed);
+        go_turnleft(speed);
         break;
     case 2:
-//        go_turnright(speed);
+        go_turnright(speed);
         break;
     case -1:
-//        go_back(speed);
+        go_back(speed);
         break;
     }
 }
-*/
 
 static void GetCommend(void *arg)
 {
@@ -79,13 +88,12 @@ static void GetCommend(void *arg)
     while (1)
     {
         // 每次清空BUF
-        //bzero(buf, SIZE1);  影响读取，总会在读取之前清空
+        // bzero(buf, SIZE1);  影响读取，总会在读取之前清空
         retval = recvfrom(sockfd, buf, SIZE1, 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
         if (retval < 0)
         {
             printf("recvfrom failed, %ld!\r\n", retval);
         }
-        //printf("发送 %s",buf);
         sem_post(&sem);
         if (!strcmp("close\n", buf))
             break;
@@ -105,54 +113,51 @@ static void UdpServer(void *arg)
     (void)arg;
     while (1)
     {
-        //printf("接收\n");
         sem_wait(&sem);
-       // printf("%s\n",buf);
         if (!strcmp("up\n", buf)) // 结束符也要比较
         {
-            // direct = 0;
-            //                go_forward(speed);
+            direct = 0;
+            go_forward(speed);
             printf("前进\n");
         }
         else if (!strcmp("back\n", buf))
         {
-            //   direct = -1;
-            //               go_back(speed);
+            direct = -1;
+            go_back(speed);
             printf("后退\n");
         }
         else if (!strcmp("left\n", buf))
         {
-            //  direct = 1;
-            //                go_turnleft(speed);
+            direct = 1;
+            go_turnleft(speed);
             printf("左转\n");
         }
         else if (!strcmp("right\n", buf))
         {
-            //               direct = 2;
-            //                go_turnright(speed);
+            direct = 2;
+            go_turnright(speed);
             printf("右转\n");
         }
         else if (!strcmp("stop\n", buf))
         {
-            //                stop();
+            stop();
             speed = 400;
             printf("停车\n");
         }
         else if (!strcmp("speedup\n", buf))
         {
-            //                ChangeSpeed(true);
+            ChangeSpeed(true);
             printf("加速\n");
         }
         else if (!strcmp("speeddown\n", buf))
         {
-            //                ChangeSpeed(false);
+            ChangeSpeed(false);
             printf("减速\n");
         }
         else if (!strcmp("auto\n", buf))
         {
-            //               forauto(10);
+            forauto(10);
         }
-        // pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -165,6 +170,7 @@ void Server(void)
     }
 }
 
+/*
 int main()
 {
 
@@ -181,3 +187,6 @@ int main()
     pthread_join(t2, NULL);
     return 0;
 }
+*/
+
+#endif
