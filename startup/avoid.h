730bffe9934b15d8d7f8191fd75d2a9e698392f6
lazy <674194901@qq.com>
2022-03-09 21:35:24 +0800

    1

--- startup/avoid.h
+++ startup/avoid.h
@@ -6,19 +6,22 @@
 void avoid(){
     //先停下来
     float leftdistance,rightdistance=0;
-    stop()
+    stop();
     set_angle(-60);
     leftdistance=getDistance();
     set_angle(60);
+    set_angle(60)
     rightdistance=getDistance();
     if(leftdistance<=rightdistance){
         //右转
         go_back(100);
-        go_turnright(300);
+        stop();
+        go_turnright(400);
     }else{
         //左转
         go_back(100);
-        go_turnright(300);
+        stop();
+        go_turnright(400);
     }
     set_angle(0);
     go_forward(400);
